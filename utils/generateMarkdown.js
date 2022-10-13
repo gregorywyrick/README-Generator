370dafcb7c840615d78395fc29d02f55f846b0c6
gregorywyrick <gregmwyrick@gmail.com>
2022-10-13 18:27:39 -0500

    markdown written

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,36 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+const renderLicenseBadge = (license) => {
+  if (license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  }
+  if (license === 'Boost Software') {
+    return '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+  }
+  if (license === 'Apache') {
+    return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  }
+  return '';
+}
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const licenseSection = (license) => {
+  if (!license) {
+    return '';
+  }
+  return '## License This project uses ${license} license.';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+${renderLicenseBadge(data.license)}
+## Table of Contents
+- [Description](#description)
+- [Installation](#intallation)
+- [Usage](#usage)
+- [Credit](#credit)
+- [License](#license)
+- [Contribution](#contribution)
+- [Test](#test)
 `;
 }
 
